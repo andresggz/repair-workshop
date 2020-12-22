9860e2820fc0f8365072b9d8c12610d6f523fb8a
Andres Grisales <andres.grisalesgg@gmail.com>
2020-12-22 17:43:12 -0500

    Create domain model of user

---
+++ src/main/java/co/edu/udea/ingweb/repairworkshop/component/user/domain/Role.java
@@ -0,0 +1,12 @@
+package co.edu.udea.ingweb.repairworkshop.component.user.domain;
+
+public enum Role {
+    GERENTE_GENERAL,
+    ASISTENTE_GERENCIA,
+    GERENTE_FINANCIERO,
+    ASISTENTE_FINANCIERO,
+    ASISTENTE_RECURSOS_HUMANOS,
+    SUPERVISOR_TALLER,
+    TECNICO_TALLER,
+    PROPIETARIO;
+}
---
+++ src/main/java/co/edu/udea/ingweb/repairworkshop/component/user/domain/User.java
@@ -0,0 +1,68 @@
+package co.edu.udea.ingweb.repairworkshop.component.user.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@Generated
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 10)
+    private String identificationCard;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 30)
+    private String names;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 30)
+    private String lastNames;
+
+    @NotBlank
+    @Size(min = 6, max = 15)
+    private String phoneNumber;
+
+    @NotBlank
+    private String profilePhoto;
+
+    @Email
+    @NotNull
+    private String primaryEmailAddress;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 8, max = 45)
+    private String password;
+
+    private Set<Role> roles;
+
+    private User createdBy;
+
+    private LocalDateTime createdAt;
+
+    private User updatedBy;
+
+    private LocalDateTime updatedAt;
+
+
+}
