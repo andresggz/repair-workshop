6dbe607e326b9158b0f2b201f5eb86573771f820
Andres Grisales <andres.grisalesgg@gmail.com>
2020-12-22 17:22:46 -0500

    Initial spring settings

---
+++ src/main/java/co/edu/udea/ingweb/repairworkshop/RepairWorkshopApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.ingweb.repairworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/udea/ingweb/repairworkshop/RepairWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udea.ingweb.repairworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
